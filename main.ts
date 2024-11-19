8d69b096c2e5c559f9363f5c8ed40329afaa6c59
Moattar Ansari <moattaransari62@gmail.com>
2024-11-19 07:41:25 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,88 @@
+// Employ data using function...
+
+let data = (  
+    Name:string,
+    Designation:string,
+    Department:string,
+    JoiningDate:string,
+    EmployeeID:string,
+    ContactInformation:string,
+    Address:string,
+    SalaryInformation:string,
+    BankAccountDetails:string,
+    EmergencyContact:string,
+    Qualifications:string,
+    Experience:string,
+    PerformanceReviews:string,
+    TrainingHistory:string,
+    LeaveDetails:string,
+    MedicalHistory:string,
+    Skills:string,
+    AttendanceRecord:string,
+    Promotions:string,
+    DisciplinaryActions:string
+    ) => {
+
+    console.log(Name)
+    console.log(Designation)
+    console.log(Department)
+    console.log(JoiningDate)
+    console.log(EmployeeID)
+    console.log(ContactInformation)
+    console.log(Address)
+    console.log(SalaryInformation)
+    console.log(BankAccountDetails)
+    console.log(EmergencyContact)    
+    console.log(Qualifications)    
+    console.log(Experience)
+    console.log(PerformanceReviews)
+    console.log(TrainingHistory)
+    console.log(LeaveDetails)
+    console.log(MedicalHistory)
+    console.log(Skills)
+    console.log(AttendanceRecord)
+    console.log(Promotions)
+    console.log(DisciplinaryActions)
+
+}
+
+data(`Employee Name: Ahmad Raza`,
+`Designation: Software Engineer`,`Department: Software Development`,
+`Joining Date: 4-3-2024`,`Employee ID: SE2020-005`,`Contact Information: 123-456-7890, Ahmad.raza@gmail.com`, `Address: 123 Main Street, Cityville, ABC123\n`,
+
+`Salary Information:\n 
+>: Basic Salary: $60,000 per annum
+>: Allowances: $5,000 per annum
+>: Total Salary: $65,000 per annum\n`,
+
+`Bank Account Details:\n
+>:Bank Name: XYZ Bank
+>:Account Number: 9876543210
+>:Routing Number: 123456789\n`,
+
+`Emergency Contact:\n
+>:Name: John Khan
+>:Relationship: Brother
+>:Contact Number: 987-654-3210\n`,
+
+`Qualifications: Bachelor's Degree in Computer Science from XYZ University\n`,
+
+`Experience: 5 years of experience in software development\n`,
+
+`Performance Reviews: Consistently meets or exceeds performance expectations\n`,
+
+`Training History: Completed various training programs on new technologies and software development methodologies\n`,
+
+`Leave Details: Entitled to 15 days of annual leave, 5 days of sick leave\n`,
+
+`Medical History: No significant medical history\n`,
+
+`Skills:\n
+>:Proficient in programming languages such as Java, Python, and JavaScript
+>:Experienced in database management and software testing\n`,
+
+`Attendance Record: Average of 98% attendance over the past year\n`,
+
+`Promotions: No promotions received yet\n`,
+
+`Disciplinary Actions: No disciplinary actions on record`)
